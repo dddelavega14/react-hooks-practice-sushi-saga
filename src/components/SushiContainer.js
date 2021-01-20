9c5099eba85a73c5513aa0f80b293609c8160ad6
dddelavega14 <david14delavega@gmail.com>
2021-01-20 12:09:13 -0500

    done

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi";
 
-function SushiContainer(props) {
+function SushiContainer({ sushis, onEatSushi }) {
+  const [index, setIndex] = useState(0);
+
+  //[1,2,3,4,5,6,7..100]
+  //[1,2,3,4]
+  //[5,6,7,8]
+
+  // pagination!
+  const firstFour = sushis.slice(index, index + 4);
+
+  const sushiItems = firstFour.map((sushi) => {
+    return <Sushi key={sushi.id} sushi={sushi} onEatSushi={onEatSushi} />;
+  });
+
+  function getNextFourSushi() {
+    // 0 -> 4 -> 8
+    setIndex(index + 4);
+  }
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiItems}
+      <MoreButton onButtonClick={getNextFourSushi} />
     </div>
   );
 }
